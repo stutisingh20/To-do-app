d44238ef90a65b43cbd563ac3fe1cc84ba317e14
stuti <134305765+stutisingh20@users.noreply.github.com>
2023-09-25 23:00:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import React from 'react'
+// import Resturant from "./component/basics/Resturant"
+// import UseState from "./component/Hooks/useState"
+// import UseEffect from './component/Hooks/useEffect'
+// import UseReducer from './component/Hooks/useReducer'
+import Todo from './component/todoreact/todo'
+
+
+
+const App = () => {
+  return (
+    <>
+      <Todo/>
+      
+    </>
+  )
+}
+
+export default App
+
